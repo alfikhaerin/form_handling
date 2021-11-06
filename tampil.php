3f4fe93b9f72ade25ccf6800db6e9201af845868
jongkodingAlfi <alfikhaerin18@gmail.com>
2021-11-06 22:18:21 +0700

    no message

---
+++ tampil.php
@@ -0,0 +1,12 @@
+<?php 
+// menangkap data nama dengan method nama
+$nama = $_GET['nama'];
+// menangkap data harga dengan method nama
+$harga = $_GET['harga'];
+ 
+// menampilkan data nama
+echo "Nama anda adalah " . $nama;
+echo "<br/>";
+// menampilkan data harga
+echo "harga barang adalah " . $harga;
+?>
